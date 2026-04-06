670fe973821e8a5a18bd22db71e807a7f25f57b1
Farhad_Karimbayli <farhad.karimbayli@gmail.com>
2026-04-06 22:04:31 +0400

    fix(ui):repaired styling and authentication infrastructure

--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
-import { getToken } from "../auth/auth";
 
 export const api = axios.create({
     baseURL: "http://localhost:5126/api",
 });
 
 api.interceptors.request.use((config) => {
-    const token = getToken();
+    const token = localStorage.getItem("token");
 
     if (token) {
+        config.headers = config.headers ?? {};
         config.headers.Authorization = `Bearer ${token}`;
     }
 
